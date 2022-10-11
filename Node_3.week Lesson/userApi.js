a97213e24fb414414dd7774590fcfcf12e7e1637
RamazanKacmaz <98986481+RamazanKacmaz@users.noreply.github.com>
2022-10-11 21:41:23 +0200

    2022js

---
+++ Node_3.week Lesson/userApi.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const User = require('./db');
+
+router.get('/', function (req, res) {
+    res.send('<h1>Hello users</h1>');
+});
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.send(error);
+    }
+    //    User.find({}, function (err, users) {})
+    //    User.find({}).then(function (users) {}).catch(function (err) {});
+});
+
+router.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const users = await User.findOne({ email: id });
+        res.send(users);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+router.put('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const user = req.body;
+
+    try {
+        const users = await User.updateOne({ _id: id }, user);
+        res.send(users);
+    } catch (error) {
+        res.send(error);
+    }
+    //    User.find({}, function (err, users) {})
+    //    User.find({}).then(function (users) {}).catch(function (err) {});
+});
+
+router.delete('/users/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const response = await User.deleteOne({ _id: id });
+        res.send(response);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+router.post('/users', async (req, res) => {
+    const userInfo = req.body;
+    const newUser = new User(userInfo);
+    try {
+        const user = await newUser.save();
+        res.send(user);
+    } catch (error) {
+        res.send(error);
+    }
+
+    // newUser.save(function (err) {
+    //     if (err) return handleError(err);
+    //     res.send(user);
+    // });
+});
+
+module.exports = router;
